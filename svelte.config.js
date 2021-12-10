6e0cec2f0f7cfd0c61b67a8aa3cfbfca5bc880b8
Matheus Fantinel <matt@fantinel.dev>
2021-12-10 19:22:47 -0300

    Add anchors to headings

--- svelte.config.js
+++ svelte.config.js
@@ -2,6 +2,8 @@ import preprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-static';
 import { mdsvex } from 'mdsvex';
 import rehypeExternalLinks from 'rehype-external-links';
+import rehypeSlug from 'rehype-slug';
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
 
 const extensions = ['.svelte', '.md'];
 
@@ -15,14 +17,26 @@ const config = {
     }),
 		mdsvex({
 			extensions: extensions,
-      rehypePlugins: [rehypeExternalLinks]
+      rehypePlugins: [
+				rehypeExternalLinks, // Adds 'target' and 'rel' to external links
+				rehypeSlug, // Adds 'id' attributes to Headings (h1,h2,etc)
+				[rehypeAutolinkHeadings, { // Adds hyperlinks to the headings, requires rehypeSlug
+					behavior: 'append',
+					content: {
+						type: 'element',
+						tagName: 'span',
+						properties: {className:['heading-link']},
+						children: [{type: 'text', value: '#'}]
+					}
+				}]
+			]
 		})
 	],
 	extensions: extensions,
 
 	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte',
+		// hydrate the <div id="svelte-root"> element in src/app.html
+		target: '#svelte-root',
 		adapter: adapter()
 	}
 };
