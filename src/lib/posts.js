a456c0921cca8ab3c4a8e32781cbd6c288d76bee
Matheus Fantinel <matt@fantinel.dev>
2021-12-10 19:25:15 -0300

    Clean up relatedPosts code

--- src/lib/posts.js
+++ src/lib/posts.js
@@ -35,6 +35,30 @@ function groupByTags(array) {
 	}, {});
 }
 
+function getRelatedPosts(post) {
+	let allRelatedPosts = [];
+	// Get posts with same tags
+	for (const tag of post.tags) {
+		allRelatedPosts = allRelatedPosts.concat(categories[tag]);
+	}
+
+	let relatedPosts = [];
+	// Sort by number of similar tags
+	for (const relPost of allRelatedPosts) {
+		if (relPost.slug === post.slug) {
+			continue;
+		}
+		const rel = relatedPosts.find(x => x.post.slug === relPost.slug);
+		if (rel) {
+			rel.count++;
+		} else {
+			relatedPosts.push({post: { ...relPost, readingTime: readingTime(relPost.html).text }, count: 1, date: relPost.date});
+		}
+	}
+
+	return relatedPosts;
+}
+
 const filteredPosts = posts
 	.filter((post) => !post.hidden)
 	.sort((a, b) =>
@@ -47,25 +71,7 @@ const filteredPosts = posts
 	.map((post) => {
 		const readingTimeDuration = readingTime(post.html).text;
 
-		let allRelatedPosts = [];
-		// Get posts with same tags
-		for (const tag of post.tags) {
-			allRelatedPosts = allRelatedPosts.concat(categories[tag]);
-		}
-
-    let relatedPosts = [];
-    // Sort by number of similar tags
-    for (const relPost of allRelatedPosts) {
-      if (relPost.slug === post.slug) {
-        continue;
-      }
-      const rel = relatedPosts.find(x => x.post.slug === relPost.slug);
-      if (rel) {
-        rel.count++;
-      } else {
-        relatedPosts.push({post: { ...relPost, readingTime: readingTime(relPost.html).text }, count: 1, date: relPost.date});
-      }
-    }
+		const relatedPosts = getRelatedPosts(post);		
 
 		return {
 			...post,
