f703b5d4ba55e3a100ec08b681eabb060d422657
myway880 <myway880@gmail.com>
2022-09-14 06:10:44 +0200

    Update libs - move prerender from config to layout

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,6 +1,7 @@
 <script>
 	import '../app.scss';
 	import { keywords } from '$lib/meta';
+	export const prerender = true;
 </script>
 
 <svelte:head>
--- svelte.config.js
+++ svelte.config.js
@@ -39,9 +39,6 @@ const config = {
 
 	kit: {
 		adapter: adapter(),
-		prerender: {
-			default: true
-		},
 		trailingSlash: 'always'
 	}
 };
