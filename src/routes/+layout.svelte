b620a8e58d1b812ee6e05b7e009da84161e0f8ca
Matt Fantinel <matt@fantinel.dev>
2023-01-26 17:37:29 -0300

    Don't overwrite meta keywords in layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,5 @@
 <script>
 	import '../app.scss';
-	import { keywords } from '$lib/meta';
 </script>
-
-<svelte:head>
-	<meta name="keywords" content={keywords.join(', ')} />
-</svelte:head>
 
 <slot />
