8afbd19195ca5bba475267ea3696e68616dc9901
Matt Fantinel <matt@fantinel.dev>
2023-02-06 21:43:58 -0300

    Redirect on client side only

--- src/routes/(waves)/404/+page.svelte
+++ src/routes/(waves)/404/+page.svelte
@@ -1,25 +1,18 @@
 <script>
 	import Button from '$lib/components/atoms/Button.svelte';
 	import Error from '$lib/icons/error.svelte';
-
-	import { page } from '$app/stores';
-	import { goto } from '$app/navigation';
-	import { browser } from '$app/environment';
-
-	if (browser && $page.status === 404) {
-		console.log($page);
-		// SvelteKit is not redirecting pages with trailing slashes like "/page/" to "/page"
-		// When they're prerendered. Doing this manually here until it's fixed.
-		// https://github.com/sveltejs/kit/issues/8839
-
-		if (window.location.pathname.endsWith('/') && window.location.pathname !== $page.url.pathname) {
-			console.log(`404 - Redirecting from ${window.location.pathname} to ${$page.url.pathname}`);
-			goto($page.url.pathname);
-		}
-	}
 </script>
 
 <div class="error-page">
+	<script>
+		console.log('ends with /', window.location.pathname.endsWith('/'));
+		if (window.location.pathname.endsWith('/')) {
+			console.log(
+				`Redirecting from ${window.location.pathname} to ${window.location.pathname.slice(0, -1)}`
+			);
+			window.location.pathname = window.location.pathname.slice(0, -1);
+		}
+	</script>
 	<div class="container">
 		<h1>Oh no! 404!</h1>
 		<div class="svg-wrapper">
