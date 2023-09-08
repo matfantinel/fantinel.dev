020563849f109838fad48d0c8c427295e876c6c0
Matt Fantinel <matt@fantinel.dev>
2023-09-08 00:36:58 -0300

    Cleanup dependencies

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,6 @@
 // import adapter from '@sveltejs/adapter-static';
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
-import { mdsvex } from 'mdsvex';
-import rehypeExternalLinks from 'rehype-external-links';
-import rehypeSlug from 'rehype-slug';
-import rehypeAutolinkHeadings from 'rehype-autolink-headings';
 
 const extensions = ['.svelte', '.md'];
 
@@ -21,30 +17,7 @@ const config = {
 			}
 		}
 	},
-	preprocess: [
-		vitePreprocess(),
-		mdsvex({
-			extensions: extensions,
-			rehypePlugins: [
-				rehypeExternalLinks, // Adds 'target' and 'rel' to external links
-				rehypeSlug, // Adds 'id' attributes to Headings (h1,h2,etc)
-				[
-					rehypeAutolinkHeadings,
-					{
-						// Adds hyperlinks to the headings, requires rehypeSlug
-						behavior: 'prepend',
-						properties: { className: ['heading-link'], title: 'Permalink', ariaHidden: 'true' },
-						content: {
-							type: 'element',
-							tagName: 'span',
-							properties: {},
-							children: [{ type: 'text', value: '#' }]
-						}
-					}
-				]
-			]
-		})
-	],
+	preprocess: [vitePreprocess()],
 	extensions: extensions
 };
 
