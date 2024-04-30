78645136c6b5b0350cedcdcda8693a8332726214
Matt Fantinel <matt@fantinel.dev>
2024-04-30 17:16:48 -0300

    Try resvg again

--- src/lib/opengraph/renderImage.js
+++ src/lib/opengraph/renderImage.js
@@ -1,3 +1,5 @@
+import satori from 'satori';
+// import { Resvg } from '@resvg/resvg-js';
 import { html as toReactNode } from 'satori-html';
 import { read } from '$app/server';
 import { ImageResponse } from '@vercel/og';
@@ -14,24 +16,7 @@ export async function componentToPng(component, props, height = 630, width = 120
 	const result = component.render(props);
 	const markup = toReactNode(`${result.html}<style>${result.css.code}</style>`);
 
-	return new ImageResponse(markup, {
-		fonts: [
-			{
-				name: 'Fixel',
-				data: await fixelBoldData,
-				style: 'normal'
-			},
-			{
-				name: 'Fixel Black',
-				data: await fixelBlackData,
-				style: 'normal'
-			}
-		],
-		height: +height,
-		width: +width
-	});
-
-	// const svg = await satori(markup, {
+	// return new ImageResponse(markup, {
 	// 	fonts: [
 	// 		{
 	// 			name: 'Fixel',
@@ -48,18 +33,35 @@ export async function componentToPng(component, props, height = 630, width = 120
 	// 	width: +width
 	// });
 
-	// const resvg = new Resvg(svg, {
-	// 	fitTo: {
-	// 		mode: 'width',
-	// 		value: +width
-	// 	}
-	// });
+	const svg = await satori(markup, {
+		fonts: [
+			{
+				name: 'Fixel',
+				data: await fixelBoldData,
+				style: 'normal'
+			},
+			{
+				name: 'Fixel Black',
+				data: await fixelBlackData,
+				style: 'normal'
+			}
+		],
+		height: +height,
+		width: +width
+	});
 
-	// const png = resvg.render();
+	const resvg = new Resvg(svg, {
+		fitTo: {
+			mode: 'width',
+			value: +width
+		}
+	});
 
-	// return new Response(png.asPng(), {
-	// 	headers: {
-	// 		'content-type': 'image/png'
-	// 	}
-	// });
+	const png = resvg.render();
+
+	return new Response(png.asPng(), {
+		headers: {
+			'content-type': 'image/png'
+		}
+	});
 }
