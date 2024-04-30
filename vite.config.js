e53317000bb792638626e78c50b65e510e43b9c2
Matt Fantinel <matt@fantinel.dev>
2024-04-30 16:44:21 -0300

    Make ReSVG a dependency

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,11 @@ import path from 'path';
 /** @type {import('vite').UserConfig} */
 const config = {
 	plugins: [sveltekit()],
+	build: {
+		rollupOptions: {
+			external: ['@resvg/resvg-js']
+		}
+	},
 	resolve: {
 		alias: {
 			$routes: path.resolve('./src/routes'),
