caf553d14667de1022e2651fa3c1933dac91f79d
Rasel Shikdar <103440692+raselshikdar@users.noreply.github.com>
2024-05-31 01:09:55 +0600

    Update +page.svelte

--- src/routes/(article)/[slug]/+page.svelte
+++ src/routes/(article)/[slug]/+page.svelte
@@ -20,7 +20,7 @@
 						alt="Rasel Shikdar"
 					/>
 					<div class="details">
-						<p class="note">by <a href="https://rsinfo.vercel.app/resume">Rasel Shikdar</a></p>
+						<p class="note">by <a href="/resume">Rasel Shikdar</a></p>
 						<p class="note">{dateformat(post.date, 'UTC:dd mmm yyyy')} - {post.readingTime}</p>
 						{#if post.updated}
 							<p class="note">Updated {dateformat(post.updated, 'UTC:dd mmm yyyy')}</p>
