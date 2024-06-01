570d2f532b29e7e00046a8f3456a67001f906447
Rasel Shikdar <103440692+raselshikdar@users.noreply.github.com>
2024-06-01 10:35:08 +0600

    Update Footer.svelte

--- src/lib/components/organisms/Footer.svelte
+++ src/lib/components/organisms/Footer.svelte
@@ -15,8 +15,8 @@
 			© {currentYear} Rasel Shikdar. Made in Bangladesh.
 		</div>
 		<div class="credits">
-			Powered by <a href="https://rsinfo.vercel.app/" target="_blank" rel="noopener noreferrer"
-				>RsInfo</a
+			Powered by <a href="https://kit.svelte.dev/" target="_blank" rel="noopener noreferrer"
+				>SvelteKit</a
 			>. Icons by
 			<a href="https://iconoir.com/" target="_blank" rel="noopener noreferrer">Iconoir</a>.
 		</div>
