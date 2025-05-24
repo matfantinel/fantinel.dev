dd9e6283b57a16477524e1f760d4815e97394566
Matt Fantinel <matt@fantinel.dev>
2025-05-24 19:08:48 -0300

    Add adapter-vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,15 @@ import { defineConfig } from 'astro/config';
 import svelte from '@astrojs/svelte';
 import yaml from '@modyfi/vite-plugin-yaml';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [svelte()],
+
   vite: {
     plugins: [yaml()]
-  }
+  },
+
+  adapter: vercel()
 });
