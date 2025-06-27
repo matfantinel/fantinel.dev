e44aa2f42f0241c5f4f18f91a6ec68c0f7698e24
Matt Fantinel <matt@fantinel.dev>
2025-06-27 10:46:19 -0300

    Cache opengraph images for 15 days

--- src/utils/opengraph.ts
+++ src/utils/opengraph.ts
@@ -9,64 +9,70 @@ import { resolve } from 'path';
 
 const rootDir = process.cwd();
 const monaspaceKryptonPath = resolve(
-  rootDir,
-  'public/og/monaspace-krypton-latin-600-normal.woff'
+	rootDir,
+	'public/og/monaspace-krypton-latin-600-normal.woff'
 );
 
 const monaSansPath = resolve(
-  rootDir,
-  'public/og/MonaSans-ExtraBold.otf'
+	rootDir,
+	'public/og/MonaSans-ExtraBold.otf'
 );
 
 const monaspaceKryptonBuffer = readFileSync(monaspaceKryptonPath);
 const monaSansBuffer = readFileSync(monaSansPath);
 
 export async function componentToPng(component: any, props: any) {
-	const container = await experimental_AstroContainer.create();
-	container.addServerRenderer({ renderer: svelteRenderer, name: "svelte" });
-	const result = await container.renderToString(component, { props });
-	const html = unescapeHtml(result);
-	const markup = toReactNode(html);
+	try {
+		const container = await experimental_AstroContainer.create();
+		container.addServerRenderer({ renderer: svelteRenderer, name: "svelte" });
+		const result = await container.renderToString(component, { props });
+		const html = unescapeHtml(result);
+		const markup = toReactNode(html);
 
-	const svg = await satori(markup, {
-		fonts: [
-			{
-				name: 'Monaspace Krypton',
-				data: monaspaceKryptonBuffer,
-				style: 'normal'
-			},
-			{
-				name: 'Mona Sans',
-				data: monaSansBuffer,
-				style: 'normal'
+		const svg = await satori(markup, {
+			fonts: [
+				{
+					name: 'Monaspace Krypton',
+					data: monaspaceKryptonBuffer,
+					style: 'normal'
+				},
+				{
+					name: 'Mona Sans',
+					data: monaSansBuffer,
+					style: 'normal'
+				}
+			],
+			width: 1200,
+			height: 630
+		});
+
+		const resvg = new Resvg(svg, {
+			fitTo: {
+				mode: 'width',
+				value: 1200
 			}
-		],
-		width: 1200,
-		height: 630
-	});
+		});
 
-	const resvg = new Resvg(svg, {
-		fitTo: {
-			mode: 'width',
-			value: 1200
-		}
-	});
+		const png = resvg.render();
 
-	const png = resvg.render();
-
-	return new Response(png.asPng(), {
-		headers: {
-			'content-type': 'image/png'
-		}
-	});
+		return new Response(png.asPng(), {
+			headers: {
+				'content-type': 'image/png',
+				'cache-control': 'public, max-age=1296000, immutable' // 15 days
+			}
+		});
+	} catch (e) {
+		console.error(e);
+		return new Response('Error rendering component', { status: 500 });
+	}
 }
 
 // https://github.com/natemoo-re/satori-html/issues/20
 function unescapeHtml(html: string) {
 	return html
-			.replace(/&amp;/g, '&')
-			.replace(/&lt;/g, '<')
-			.replace(/&gt;/g, '>')
-			.replace(/&quot;/g, '"')
-			.replace(/&#39;/g, "'");
+		.replace(/&amp;/g, '&')
+		.replace(/&lt;/g, '<')
+		.replace(/&gt;/g, '>')
+		.replace(/&quot;/g, '"')
+		.replace(/&#39;/g, "'");
 }
