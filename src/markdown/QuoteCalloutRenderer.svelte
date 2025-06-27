2f6eae9aed360bc84c2fa22d09ee94819421a213
Matt Fantinel <matt@fantinel.dev>
2025-06-26 22:43:19 -0300

    Render callouts better

--- src/markdown/QuoteCalloutRenderer.svelte
+++ src/markdown/QuoteCalloutRenderer.svelte
@@ -11,38 +11,20 @@
 	// So, we need to check if the text starts with that pattern
 	// And get the type
 
-	// But first, we need to remove the "\" character from the beginning of the string
-	// That Tina CMS adds before the callout pattern
-	text = text.replace(/^\\/g, '');
-
 	const isCallout = /^\[!([a-z]+)\]/i.test(text);
 	const calloutType = isCallout ? text.match(/^\[!([a-z]+)\]/i)?.[1] : undefined;
 	
 	if (isCallout) {
 		text = text.replace(/^\[!\w+\]\s?/, '');
 	}
-
-	// Remove the line breaks (\n) from beginning and end of the string
-	text = text.replace(/^\n|\n$/g, '');
-
-	// Now, replace the remaining line breaks with <br> tags
-	text = text.replace(/\n/g, '<br>');
-
-	// #region Handle Tina CMS editor quirks
-
-	// Remove every "\" character immediately before or after a <br> tag
-	text = text.replace(/\\<br>/g, '<br>');
-	text = text.replace(/<br>\\/g, '<br>');
-
-	// #endregion
 </script>
 
 {#if isCallout}
-	<Callout type={calloutType}>
-		<MarkdownRenderer content={text} isInline />
+	<Callout type={calloutType as 'info' | 'warning' | 'success'}>
+		<MarkdownRenderer content={text} />
 	</Callout>
 {:else}
 	<Blockquote>
-		<MarkdownRenderer content={text} isInline />
+		<MarkdownRenderer content={text} />
 	</Blockquote>
 {/if}
